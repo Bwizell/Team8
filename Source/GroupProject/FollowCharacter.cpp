642eb1d09bc879b77f4ba15ab1039bec9bbae374
Makahox <Makahox@Gmail.com>
2024-01-11 09:36:28 +0000

    initial commit

--- Source/GroupProject/FollowCharacter.cpp
+++ Source/GroupProject/FollowCharacter.cpp
@@ -6,17 +6,12 @@
 // Sets default values
 AFollowCharacter::AFollowCharacter()
 {
-<<<<<<< HEAD
+
 
 	PrimaryActorTick.bCanEverTick = true;
 
 }
-=======
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
 
->>>>>>> 7e5d3856fb6a0ec672e058c788a16378e5522142
 // Called when the game starts or when spawned
 void AFollowCharacter::BeginPlay()
 {
